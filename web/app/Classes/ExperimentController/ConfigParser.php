57e64c5285e36f3b396989a568addd959b5c68a3
bozidars27 <bozho93@gmail.com>
2019-03-01 17:19:43 +0100

    Issue #14: Updates general data retrieval logic according to the modifications made in `interface.py` Python module

--- web/app/Classes/ExperimentController/ConfigParser.php
+++ web/app/Classes/ExperimentController/ConfigParser.php
@@ -5,36 +5,17 @@ namespace App\Classes\ExperimentController;
 class ConfigParser {
 
 	private $scenario_config;
-	private $scenarios = [
-		"building-automation"   => "Building automation", 
-		"home-automation"       => "Home automation", 
-		"industrial-monitoring" => "Industrial monitoring"];
-	private $testbeds  = [
-		"iotlab" => "IoT-LAB",
-		"wilab"  => "w-iLab.t"
-	];
 	
-	function get_config_data() {
-		$res["scenarios"] = $this->scenarios;
-		$res["testbeds"]  = $this->testbeds;
-
-		foreach ($this->scenarios as $sc_identifier => $scenario) {
-			$res["nodes"][$sc_identifier] = [];
-			foreach ($this->testbeds as $tb_identifier => $testbed) {
-				$res["nodes"][$sc_identifier][$tb_identifier] = json_decode($this->_invoke_python_interface(
-					$sc_identifier,
-					$tb_identifier
-				));
-			}
-		}
-
-		return $res;
+	function get_config_data($param, $scenario, $testbed) {
+		return $this->_invoke_python_interface($param, $scenario, $testbed);
 	}
 
-	private function _invoke_python_interface($sc_identifier, $tb_identifier) {
+	private function _invoke_python_interface($param, $scenario, $testbed) {
 		$python_interface_path = base_path() . "/../scenario-config/interface.py";
-		$command = "python $python_interface_path --scenario=$sc_identifier --testbed=$tb_identifier";
 
-		return shell_exec($command);
+		$command = "python $python_interface_path --param=$param";
+		$command .= $param == 'nodes' ? " --scenario=$scenario --testbed=$testbed" : "";
+
+		return json_decode(shell_exec($command));
 	}
 }
